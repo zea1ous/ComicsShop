cbbd0b0dda34ad645f0babfe3315e1110058b8fc
Alexander Kolovatov <a.kolovatov@gmail.com>
2019-07-27 23:03:01 +0600

    Initial Commit

---
+++ ComicsShop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ComicsShop
+//
+//  Created by Alex Kolovatov on 27/07/2019.
+//  Copyright © 2019 Alex Kolovatov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
